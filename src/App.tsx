24a9fff42571f481d826fa40c2859a87e0d89ec4
Fernando González <fernandomenjivar2011@gmail.com>
2025-09-12 22:58:26 -0600

    chore: add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Converter from "@/pages/converter";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
     <TooltipProvider>
       <Toaster />
       <Converter />
+      <Analytics />
     </TooltipProvider>
   );
 }
